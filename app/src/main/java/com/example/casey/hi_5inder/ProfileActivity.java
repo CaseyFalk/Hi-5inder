c0347555cae87f6ad6cc3f96bd6618466465b7be
CaseyFalk <caseyfalkowski@gmail.com>
2018-09-22 16:29:43 -0400

    glide

--- app/src/main/java/com/example/casey/hi_5inder/ProfileActivity.java
+++ app/src/main/java/com/example/casey/hi_5inder/ProfileActivity.java
@@ -2,6 +2,9 @@ package com.example.casey.hi_5inder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        //Initialize ImageView
+        ImageView imageView3 = (ImageView) findViewById(R.id.imageView);
+
+        //Loading image from below url into imageView
+        Glide.with(this)
+                .load("gs://hi-5inder.appspot.com/036.png")
+                .into(imageView3);
+
     }
 }

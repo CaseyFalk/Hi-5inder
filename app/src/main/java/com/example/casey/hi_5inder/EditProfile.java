b46ce69f66a8af336887882f91b3d2f26ddd703e
kingc95 <cmacks95@gmail.com>
2018-09-23 08:02:23 -0400

    request Camera

--- app/src/main/java/com/example/casey/hi_5inder/EditProfile.java
+++ app/src/main/java/com/example/casey/hi_5inder/EditProfile.java
@@ -1,13 +1,23 @@
 package com.example.casey.hi_5inder;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.provider.SyncStateContract;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
@@ -34,6 +44,9 @@ import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
 
+import static android.app.PendingIntent.getActivity;
+
+
 public class EditProfile extends AppCompatActivity implements View.OnClickListener{
 
     private Button saveProfileButton;
@@ -41,6 +54,7 @@ public class EditProfile extends AppCompatActivity implements View.OnClickListen
     private EditText editUsername;
     private FirebaseAuth firebaseAuth;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int PERMISSION_REQUEST_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -101,6 +115,7 @@ public class EditProfile extends AppCompatActivity implements View.OnClickListen
         profilePic.setOnClickListener(this);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onClick(View v) {
         if (v == saveProfileButton){
@@ -116,6 +131,17 @@ public class EditProfile extends AppCompatActivity implements View.OnClickListen
 
         }
         else if (v == profilePic){
+
+            if (checkPermission()) {
+                //main logic or main code
+
+                // . write your main code to execute, It will execute if the permission is already given.
+
+            } else {
+                requestPermission();
+            }
+
+
             Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             if(takePicture.resolveActivity(getPackageManager()) != null){
                 startActivityForResult(takePicture, REQUEST_IMAGE_CAPTURE);
@@ -147,5 +173,59 @@ public class EditProfile extends AppCompatActivity implements View.OnClickListen
     private Bitmap base64ToBitmap(String b64) {
         byte[] imageAsBytes = Base64.decode(b64.getBytes(), Base64.DEFAULT);
         return BitmapFactory.decodeByteArray(imageAsBytes, 0, imageAsBytes.length);
+    }
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA},
+                PERMISSION_REQUEST_CODE);
+    }
+
+    private boolean checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+
+                    // main logic
+                } else {
+                    Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                                != PackageManager.PERMISSION_GRANTED) {
+                            showMessageOKCancel("You need to allow access permissions",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermission();
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
     }
 }
